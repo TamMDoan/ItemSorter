33e3a48a2fa4f5aecc7b526dc84dae98917be3d0
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-11-23 22:05:00 -0500

    item, idcomparator complete; ask about the tests for nameComparator

--- src/main/java/rocks/zipcode/io/comparators/IdComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/IdComparator.java
@@ -1,7 +1,24 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+import java.util.Objects;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class IdComparator {
+public class IdComparator implements Comparator<Item> {
+
+    @Override
+    public int compare(Item o1, Item o2) {
+//        if(o1.getId() < o2.getId()){
+//            return -1;
+//        }
+//        else if(Objects.equals(o1.getId(), o2.getId())){
+//            return 0;
+//        }
+//        return Integer.parseInt(o1.getId().toString());
+        return (int) (o1.getId() - o2.getId());
+    }
 }
--- src/main/java/rocks/zipcode/io/comparators/NameComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/NameComparator.java
@@ -1,7 +1,23 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class NameComparator {
+public class NameComparator implements Comparator<Item> {
+    @Override
+    public int compare(Item o1, Item o2) {
+        if(o1.getName().toLowerCase().equals(o2.getName())){
+            return 0;
+        }
+        else if(o1.getName().length() > o2.getName().length()){
+            return 1;
+        }
+
+        return -1;
+
+    }
 }
