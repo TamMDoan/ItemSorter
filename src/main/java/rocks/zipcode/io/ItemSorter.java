59be413a9b0c7220a012f0ab9a0c5ceaee540531
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-11-23 23:02:40 -0500

    sortbyid passing, names not passing prob bc wrong logic for nameComparator

--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -6,10 +6,22 @@ import java.util.Comparator;
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+    Item[] items;
     public ItemSorter(Item[] items) {
+        this.items = items;
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+        for(int i = 0; i < this.items.length - 1; i++){
+            for(int j = i+1; j < this.items.length; j++){
+                if(comparator.compare(this.items[i], this.items[j]) > 0){
+                    Item temp = this.items[i];
+                    this.items[i] = this.items[j];
+                    this.items[j] = temp;
+                }
+            }
+        }
+        System.out.println(this.items[0].getName() + " " + this.items[1].getName() + " " + this.items[2].getName());
+        return this.items;
     }
 }
