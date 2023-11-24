943081a4bfe03cc3e2ab966eb614eb4ae127f4d9
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-11-23 22:10:55 -0500

    priceComparator completed

--- src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
@@ -1,7 +1,25 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+import java.util.Objects;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class PriceComparator {
+public class PriceComparator implements Comparator<Item> {
+    @Override
+    public int compare(Item o1, Item o2) {
+        System.out.println(o1.getPrice());
+        System.out.println(o2.getPrice());
+        if(o1.getPrice() == o2.getPrice()){
+            return 0;
+        }
+        else if(o1.getPrice() > o2.getPrice()){
+            return 1;
+        }
+
+        return -11;
+    }
 }
