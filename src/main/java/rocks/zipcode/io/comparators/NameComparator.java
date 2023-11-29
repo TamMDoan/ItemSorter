996a2037c245c209eedfcd5d0ba5b00438507108
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-11-29 18:37:27 -0500

    FINISHED, rejoice

--- src/main/java/rocks/zipcode/io/comparators/NameComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/NameComparator.java
@@ -13,11 +13,29 @@ public class NameComparator implements Comparator<Item> {
         if(o1.getName().toLowerCase().equals(o2.getName())){
             return 0;
         }
-        else if(o1.getName().length() > o2.getName().length()){
-            return 1;
-        }
 
+        // COMPARE ASCII OF EACH INDIVIDUAL LETTER DUMMY
+        for(int i = 0; i < o1.getName().length(); i++){
+            if(o1.getName().charAt(i) > o2.getName().charAt(i)){
+                return 1;
+            }
+            else if(o1.getName().charAt(i) < o2.getName().charAt(i)){
+                break;
+            }
+        }
         return -1;
 
     }
+
+//    @Override
+//    public int compare(Item o1, Item o2){
+//        if(o1.getName().equals(o1.getName())){
+//            return 0;
+//        }
+//        else if(o1.get > o2.getId()){
+//            return 1;
+//        }
+//
+//        return -1;
+//    }
 }
